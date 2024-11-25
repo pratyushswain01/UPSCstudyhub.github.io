d1779f30e0f3353efd84807565937145008be0a9
pratyushswain01 <pratyushkumarswain2005@gmail.com>
2024-11-25 19:23:46 +0530

    Update newspaperscript.js

--- newspaperscript.js
+++ newspaperscript.js
@@ -1,1 +1,10 @@
+// Optional JavaScript to pause ticker on hover
+const ticker = document.querySelector('.ticker');
 
+ticker.addEventListener('mouseover', function() {
+  ticker.style.animationPlayState = 'paused';
+});
+
+ticker.addEventListener('mouseout', function() {
+  ticker.style.animationPlayState = 'running';
+});
