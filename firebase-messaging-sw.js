52bd069933225ae514617bbe0c30fa93d7969b16
pratyushswain01 <pratyushkumarswain2005@gmail.com>
2026-01-15 00:14:13 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
+// Initialize with your project config
 firebase.initializeApp({
   apiKey: "AIzaSyBoPtr3d8se-fcGxkzixG8OE7dfVyjPcs4",
   projectId: "upscstudyhub-github",
@@ -10,11 +11,12 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
+// Handle background notifications
 messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: 'images/LOGO.jpg'
+    icon: 'images/LOGO.jpg' 
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
